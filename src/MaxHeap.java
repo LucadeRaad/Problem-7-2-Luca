1b129346eddf26a61312c991354074f5ab2e320f
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-03-10 18:06:01 -0700

    Created the logn and nlogn functions. Still need to implement tests and finish remaining hw functions

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -8,17 +8,47 @@ public class MaxHeap implements Heap {
         this.size = size;
         data = new Integer[size];
     }
+    // helper method that adds elements to a heap for O(n)
+    private void arrayHeapSort(int index) {
+        // Gives first child, other is one index point down
+        int children = (index * 2) + 1;
+        while(children < data.length) {
+            if(data[children] > data[index]) {
+                swap(children, index);
+            } else if(children + 1 < data.length && data[children + 1] > data[index]) {
+                swap(children, index);
+            }
+        }
+    }
+
+    // Simple swap function
+    private void swap(int numOne, int numTwo) {
+        int temp = data[numOne];
+        data[numOne] = data[numTwo];
+        data[numTwo] = temp;
+    }
 
     // build a heap based on data
     // to be implemented in O(nlogn)
     public void MaxHeapLogN(Integer[] data) {
-        // homework
+        // Using prebuilt heap from java
+        MaxHeap heap = new MaxHeap(data.length);
+        for(Integer input: data)
+            heap.add(input);
+        int i = 0;
+        // Need to seperate these because heap needs to sort(in nlogn fasion) the elements
+        for(Integer input: heap.data) {
+            data[i] = input;
+            i++;
+        }
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
-        // homework
+        for(int i = 0; i < data.length; i++) {
+            arrayHeapSort(i);
+        }
     }
 
     // add an item to the heap
