94fb7c8912c11387247587f6a5171e98e10e7a56
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-03-11 19:09:38 -0700

    the test case for logn and n, need to fix logn and n code

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,27 +1,38 @@
+import javax.management.openmbean.OpenMBeanInfoSupport;
+
 public class MaxHeap implements Heap {
-    int capacity;
     int size;
 
     Integer[] data;
 
-    public MaxHeap(int size) {
-        this.size = size;
-        data = new Integer[size];
+    public MaxHeap(int capacity) {
+        data = new Integer[capacity];
+        size = 0;
     }
+
     // helper method that adds elements to a heap for O(n)
     private void arrayHeapSort(int index) {
-        // Gives first child, other is one index point down
-        int children = (index * 2) + 1;
-        while(children < data.length) {
-            if(data[children] > data[index]) {
-                swap(children, index);
-            } else if(children + 1 < data.length && data[children + 1] > data[index]) {
-                swap(children, index);
+        // Have the children of the index
+        int leftChild = (index * 2) + 1;
+        int rightChild = (index * 2) + 2;
+
+        // Recursively checks the child nodes to see if
+        if(leftChild < data.length) {
+            System.out.println(index + " <index leftChild> " + leftChild);
+            //System.out.println(data.length);
+            if (data[leftChild] > data[index]) {
+                swap(leftChild, index);
+                arrayHeapSort(leftChild);
+            }
+
+            if (rightChild < data.length && data[rightChild] > data[index]) {
+                swap(rightChild, index);
+                arrayHeapSort(rightChild); // Some duplicate code
             }
         }
     }
 
-    // Simple swap function
+    // Swaps elements in an array
     private void swap(int numOne, int numTwo) {
         int temp = data[numOne];
         data[numOne] = data[numTwo];
@@ -31,29 +42,28 @@ public class MaxHeap implements Heap {
     // build a heap based on data
     // to be implemented in O(nlogn)
     public void MaxHeapLogN(Integer[] data) {
-        // Using prebuilt heap from java
-        MaxHeap heap = new MaxHeap(data.length);
-        for(Integer input: data)
-            heap.add(input);
-        int i = 0;
-        // Need to seperate these because heap needs to sort(in nlogn fasion) the elements
-        for(Integer input: heap.data) {
-            data[i] = input;
-            i++;
-        }
+        // Idea is that it adds, haven't added adding yet
+        for(Integer input: this.data)
+            add(input);
+
+        //size = this.data.length;
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
+        // Need a deep copy of the array, this MaxHeap object now holds the array and we manipulate our own data
+        System.arraycopy(data, 0, this.data, 0, data.length);
+
         for(int i = 0; i < data.length; i++) {
-            arrayHeapSort(i);
+            arrayHeapSort(this.data[this.data.length - 1 - i]);
         }
+        size = this.data.length;
     }
 
     // add an item to the heap
     public boolean add(Integer item) {
-        // homework
+
         return false;
     }
 
@@ -67,5 +77,25 @@ public class MaxHeap implements Heap {
     public Integer pop() {
         // homework
         return null;
+    }
+
+    // Built the recommended equals function
+    public boolean equals(MaxHeap other) {
+        // Null pointer exceptions, just in case
+        if(other.data.length != data.length) return false;
+
+        //System.out.println();
+//        for(int count = 0; count < other.data.length; count++) {
+//            //System.out.println(count);
+//            System.out.println(other.data[count]);
+//            System.out.println(data[count]);
+//        }
+
+        for(int i = 0; i < data.length; i++) {
+            if (!data[i].equals(other.data[i])) return false;
+            System.out.println(i);
+        }
+
+        return true;
     }
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,5 +1,47 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class MaxHeapTest {
 
+    @Test
+    public void HeapLogNHeapN() {
+        MaxHeap[] heapLogN = new MaxHeap[10];
+        MaxHeap[] heapN = new MaxHeap[10];
+        Random rand = new Random();
+        // Heaps in this case are expressed with arrays
+        Integer[][] testHeapLogN = new Integer[10][10];
+        Integer[][] testHeapN = new Integer[10][10];
+        // Randomly generated inputs
+        for (int i = 0; i < testHeapLogN.length; i++) {
+            heapLogN[i] = new MaxHeap(10);
+            heapN[i] = new MaxHeap(10);
+            for (int j = 0; j < testHeapLogN[i].length; j++) {
+                int same = rand.nextInt(40);
+                testHeapLogN[i][j] = same;
+                testHeapN[i][j] = same;
+                //System.out.println(same);
+            }
+            //System.out.println(i);
+            // Creating the two arrays of heaps that should be identical
+            heapLogN[i].MaxHeapLogN(testHeapLogN[i]);
+            heapN[i].MaxHeapN(testHeapN[i]);
+
+            System.out.println();
+            for(int count = 0; count < heapLogN[i].size; count++) {
+                System.out.println("Heap logN");
+                System.out.println(heapLogN[i].data[count]);
+            }
+
+            for(int count = 0; count < heapLogN[i].size; count++) {
+                System.out.println("Heap N");
+                System.out.println(heapN[i].data[count]);
+            }
+
+            assert(heapLogN[i].equals(heapN[i]));
+        }
+    }
 }
